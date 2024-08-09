e88f1b31849a713ff419c6f827a43346874932b5
Leonardo Benitez <lsbenitezpereira@gmail.com>
2024-08-09 14:24:13 +0300

    function calling (or lack of) compatible with LlamaCPP

--- libs/utils/connector_llm.py
+++ libs/utils/connector_llm.py
@@ -5,6 +5,10 @@ from functools import cached_property
 from abc import ABC, abstractmethod
 import openai
 from libs.utils.prompt_manipulation import DefinitionOpenaiTool
+from libs.utils.logger import get_logger
+
+logger = get_logger('libs.connector_llm')
+
 '''
 TODO:
 
@@ -26,6 +30,7 @@ probably the ToolCall is more reliable, so this module should be refactored to d
 
 class Capabilities(BaseModel):
     response_json_only: bool = False
+    tool_call: bool = False
     local: bool = False
 
 ###
@@ -42,7 +47,16 @@ class HyperparametersOpenAI(Hyperparameters):
     frequency_penalty: float = 0.0
     presence_penalty: float = 0.0
     stop: Optional[List[str]] = None
-    tool_choice: str = "required"
+    tool_choice: Optional[str] = "required"
+
+
+class HyperparametersLlamaCPP(Hyperparameters):
+    max_tokens: int = 150
+    temperature: float = 0.7
+    top_p: float = 1.0
+    frequency_penalty: float = 0.0
+    presence_penalty: float = 0.0
+    stop: Optional[List[str]] = None
 
 ###
 
@@ -132,35 +146,35 @@ class ConnectorLLM(BaseModel, ABC):
     credentials: Credentials
 
     @abstractmethod
-    def chat_completion(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> ChatCompletion:
+    def chat_completion(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> ChatCompletion:
         pass
 
     @abstractmethod
-    def chat_completion_stream(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> Generator[ChatCompletion, None, None]:
+    def chat_completion_stream(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> Generator[ChatCompletion, None, None]:
         pass
 
     @abstractmethod
-    async def chat_completion_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> Awaitable[ChatCompletion]:
+    async def chat_completion_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> Awaitable[ChatCompletion]:
         pass
 
     @abstractmethod
-    async def chat_completion_stream_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> AsyncGenerator[ChatCompletion, None]:
+    async def chat_completion_stream_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> AsyncGenerator[ChatCompletion, None]:
         pass
 
     @abstractmethod
-    def text_completion(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> TextCompletion:
+    def text_completion(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> TextCompletion:
         pass
 
     @abstractmethod
-    def text_completion_stream(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> Generator[TextCompletion, None, None]:
+    def text_completion_stream(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> Generator[TextCompletion, None, None]:
         pass
 
     @abstractmethod
-    async def text_completion_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> Awaitable[TextCompletion]:
+    async def text_completion_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> Awaitable[TextCompletion]:
         pass
 
     @abstractmethod
-    async def text_completion_stream_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> AsyncGenerator[TextCompletion, None]:
+    async def text_completion_stream_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> AsyncGenerator[TextCompletion, None]:
         pass
 
 
@@ -194,24 +208,26 @@ class ConnectorLLMOpenAI(ConnectorLLM):
             api_key=self.credentials.api_key,
         )
 
-    def chat_completion(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> ChatCompletion:
-        chat_completion = self.client.chat.completions.create(
-            model=self.modelname,
-            messages=[m.dict() for m in messages],
-            tools=[d.dict() for d in tool_definitions],
-            stream=False,
+    def _chat_completion_assemble_params(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> dict:
+        params = {
+            "model": self.modelname,
+            "messages": [m.dict() for m in messages],
             **self.hyperparameters.dict(),
-        )
+        }
+        if len(tool_definitions) > 0:
+            if self.capabilities.tool_call:
+                params['tools'] = [d.dict() for d in tool_definitions]
+            else:
+                logger.warning("Tool calls are not supported for this model")
+        return params
+
+    def chat_completion(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> ChatCompletion:
+        chat_completion = self.client.chat.completions.create(stream=False, **self._chat_completion_assemble_params(messages, tool_definitions))
         return ChatCompletion(**chat_completion.dict())
 
-    def chat_completion_stream(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> Generator[ChatCompletionPart, None, None]:
-        chat_completion_stream = self.client.chat.completions.create(
-            model=self.modelname,
-            messages=[m.dict()for m in messages],
-            tools=[d.dict() for d in tool_definitions],
-            stream=True,
-            **self.hyperparameters.dict(),
-        )
+    def chat_completion_stream(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> Generator[ChatCompletionPart, None, None]:
+        chat_completion_stream = self.client.chat.completions.create(stream=True, **self._chat_completion_assemble_params(messages, tool_definitions))
+
         for chunk in chat_completion_stream:
             if len(chunk.choices) > 0:
                 messages_out: List[dict] = [c.delta.dict() for c in chunk.choices]
@@ -220,24 +236,12 @@ class ConnectorLLMOpenAI(ConnectorLLM):
                 choices: List[dict] = [{'message': m} for m in messages_out]
                 yield ChatCompletionPart(choices=choices)
 
-    async def chat_completion_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> Awaitable[ChatCompletion]:
-        chat_completion = await self.client_async.chat.completions.create(
-            model=self.modelname,
-            messages=[m.dict()for m in messages],
-            tools=[d.dict() for d in tool_definitions],
-            stream=False,
-            **self.hyperparameters.dict(),
-        )
+    async def chat_completion_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> Awaitable[ChatCompletion]:
+        chat_completion = await self.client_async.chat.completions.create(stream=False, **self._chat_completion_assemble_params(messages, tool_definitions))
         return ChatCompletion(**chat_completion.dict())
 
-    async def chat_completion_stream_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool]) -> AsyncGenerator[ChatCompletionPart, None]:
-        chat_completion_stream = await self.client_async.chat.completions.create(
-            model=self.modelname,
-            messages=[m.dict()for m in messages],
-            tools=[d.dict() for d in tool_definitions],
-            stream=True,
-            **self.hyperparameters.dict(),
-        )
+    async def chat_completion_stream_async(self, messages: List[ChatCompletionMessage], tool_definitions: List[DefinitionOpenaiTool] = []) -> AsyncGenerator[ChatCompletionPart, None]:
+        chat_completion_stream = await self.client_async.chat.completions.create(stream=True, **self._chat_completion_assemble_params(messages, tool_definitions))
         async for chunk in chat_completion_stream:
             if len(chunk.choices) > 0:
                 messages: List[dict] = [c.delta.dict() for c in chunk.choices]
@@ -246,16 +250,16 @@ class ConnectorLLMOpenAI(ConnectorLLM):
                 choices: List[dict] = [{'message': m} for m in messages]
                 yield ChatCompletionPart(choices=choices)
 
-    def text_completion(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> TextCompletion:
+    def text_completion(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> TextCompletion:
         raise NotImplementedError()
 
-    def text_completion_stream(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> Generator[TextCompletion, None, None]:
+    def text_completion_stream(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> Generator[TextCompletion, None, None]:
         raise NotImplementedError()
 
-    async def text_completion_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> Awaitable[TextCompletion]:
+    async def text_completion_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> Awaitable[TextCompletion]:
         raise NotImplementedError()
 
-    async def text_completion_stream_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool]) -> AsyncGenerator[TextCompletion, None]:
+    async def text_completion_stream_async(self, prompt: str, tool_definitions: List[DefinitionOpenaiTool] = []) -> AsyncGenerator[TextCompletion, None]:
         raise NotImplementedError()
 
 
@@ -279,12 +283,13 @@ class ConnectorLLMAzureOpenAI(ConnectorLLMOpenAI):
         )
 
 
-class ConnectorLlamaCPP(ConnectorLLM):
-    pass
+class ConnectorLLMLlamaCPP(ConnectorLLMOpenAI):
+    hyperparameters: HyperparametersLlamaCPP
 
 
 def create_connector_llm(
     credentials: Credentials,
+    hyperparameters: dict = {},
     provider: Literal['openai', 'azure_openai', 'llama_cpp'] = 'openai',
     modelname: str = 'gpt-3.5-turbo',
     version: str = '1106',
@@ -298,91 +303,152 @@ def create_connector_llm(
         capabilities = Capabilities()
         if version == '1106' or version == '0125':
             capabilities.response_json_only = True
+            capabilities.tool_call = True
 
         return ConnectorLLMOpenAI(
             modelname=modelname,
             capabilities=capabilities,
-            hyperparameters=HyperparametersOpenAI(),
+            hyperparameters=HyperparametersOpenAI(**hyperparameters),
             credentials=credentials,
         )
     elif provider == 'azure_openai':
         capabilities = Capabilities()
         if version == '1106' or version == '0125':
             capabilities.response_json_only = True
+            capabilities.tool_call = True
 
         return ConnectorLLMAzureOpenAI(
             modelname=modelname,
             capabilities=capabilities,
-            hyperparameters=HyperparametersOpenAI(),
+            hyperparameters=HyperparametersOpenAI(**hyperparameters),
             credentials=credentials,
         )
     elif provider == 'llama_cpp':
-        raise NotImplementedError()
+        capabilities = Capabilities(local=True)
+        return ConnectorLLMLlamaCPP(
+            modelname=modelname,
+            capabilities=capabilities,
+            hyperparameters=HyperparametersLlamaCPP(**hyperparameters),
+            credentials=credentials,
+        )
     else:
         raise ValueError(f"Unknown provider: {provider}")
 
 
-# Pseudo tests 1 - Direct messages
-# Does not really work anymore, since I'm forcing function call
-# This can easily be refactored to make function call optional
-
+# Pseudo integration tests 1A - Simple messages - OpenAI
+# this requires calls to openAI ($$$)
 '''
-# this requires calls to openAI
-#llm = create_connector_llm(
-#    provider='azure_openai',
-#    modelname=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_CHAT"),
-#    version=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_CHAT").split('-')[-1],
-#    credentials=CredentialsOpenAI(
-#        base_url=os.getenv("AZURE_OPENAI_ENDPOINT"),
-#        api_key=os.getenv("AZURE_OPENAI_KEY"),
-#    ),
-#)
-
 llm = create_connector_llm(
-    provider='openai',
+    provider='azure_openai',
+    modelname=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_CHAT"),
+    version=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_CHAT").split('-')[-1],
+    credentials=CredentialsOpenAI(
+        base_url=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_KEY"),
+    ),
+    hyperparameters = {'max_tokens': 3, 'tool_choice': None}
+)
+
+
+# chat completion, normal
+chat_completion = llm.chat_completion(
+    messages=[ChatCompletionMessage(content='hi', role='user')],
+)
+assert type(chat_completion.choices[0].message.content) == str
+assert len(chat_completion.choices[0].message.content) > 2
+print(chat_completion.choices[0].message.content)
+
+# chat completion, stream
+chat_completion_stream = llm.chat_completion_stream(
+    messages=[ChatCompletionMessage(content='hi', role='user')],
+)
+answer = ''
+for chunk in chat_completion_stream:
+    if len(chunk.choices) > 0:
+        assert type(chunk.choices[0].message.content) == str
+        print(chunk.choices[0].message.content, end='')
+        answer += chunk.choices[0].message.content
+assert len(answer) > 2
+
+# chat completion, normal, async
+chat_completion = await llm.chat_completion_async(
+    messages=[ChatCompletionMessage(content='hi', role='user')],
+)
+assert type(chat_completion.choices[0].message.content) == str
+assert len(chat_completion.choices[0].message.content) > 2
+print(chat_completion.choices[0].message.content)
+
+# chat completion, stream, async
+chat_completion_stream = llm.chat_completion_stream_async(
+    messages=[ChatCompletionMessage(content='hi', role='user')],
+)
+answer = ''
+async for chunk in chat_completion_stream:
+    if len(chunk.choices) > 0:
+        assert type(chunk.choices[0].message.content) == str
+        print(chunk.choices[0].message.content, end='')
+        answer += chunk.choices[0].message.content
+assert len(answer) > 2
+'''
+
+
+# Pseudo integration tests 1B - Simple messages - LlamaCPP
+'''
+llm = create_connector_llm(
+    provider='llama_cpp',
     modelname='not-needed',
     version='not-needed',
     credentials=CredentialsOpenAI(
         base_url='http://llm-server:8080/v1',
         api_key='not-needed',
     ),
+    hyperparameters = {'max_tokens': 10}
 )
 
-
-# text completion, normal
+# chat completion, normal
 chat_completion = llm.chat_completion(
     messages=[ChatCompletionMessage(content='hi', role='user')],
 )
-chat_completion.choices[0].message.content
-
+assert type(chat_completion.choices[0].message.content) == str
+assert len(chat_completion.choices[0].message.content) > 2
+print(chat_completion.choices[0].message.content)
 
 # chat completion, stream
 chat_completion_stream = llm.chat_completion_stream(
     messages=[ChatCompletionMessage(content='hi', role='user')],
 )
+answer = ''
 for chunk in chat_completion_stream:
     if len(chunk.choices) > 0:
+        assert type(chunk.choices[0].message.content) == str
         print(chunk.choices[0].message.content, end='')
+        answer += chunk.choices[0].message.content
+assert len(answer) > 2
 
-
-# text completion, normal, async
+# chat completion, normal, async
 chat_completion = await llm.chat_completion_async(
     messages=[ChatCompletionMessage(content='hi', role='user')],
 )
-chat_completion.choices[0].message.content
-
+assert type(chat_completion.choices[0].message.content) == str
+assert len(chat_completion.choices[0].message.content) > 2
+print(chat_completion.choices[0].message.content)
 
 # chat completion, stream, async
 chat_completion_stream = llm.chat_completion_stream_async(
     messages=[ChatCompletionMessage(content='hi', role='user')],
 )
+answer = ''
 async for chunk in chat_completion_stream:
     if len(chunk.choices) > 0:
+        assert type(chunk.choices[0].message.content) == str
         print(chunk.choices[0].message.content, end='')
+        answer += chunk.choices[0].message.content
+assert len(answer) > 2
 '''
 
 
-# Pseudo tests 2 - Function calls
+# Pseudo integration tests 2A - Tool calling - OpenAI
+# this requires calls to openAI ($$$)
 '''
 llm = create_connector_llm(
     provider='azure_openai',
@@ -394,7 +460,6 @@ llm = create_connector_llm(
     ),
 )
 
-
 # chat completion, normal, with function call
 chat_completion = llm.chat_completion(
     messages=[
@@ -404,7 +469,8 @@ chat_completion = llm.chat_completion(
     tool_definitions = openai_function_to_tool.generate_definitions(semantic_kernel_v0_to_openai_function.generate_definitions([(PluginCapital(), "PluginCapital")])),
 )
 print(chat_completion.choices[0].message.tool_calls[0].dict())
-
+assert len(chat_completion.choices[0].message.tool_calls) > 0
+assert 'PluginCapital' in chat_completion.choices[0].message.tool_calls[0].function.name
 
 # chat completion, stream, with function call
 chat_completion_stream = llm.chat_completion_stream(
@@ -414,10 +480,18 @@ chat_completion_stream = llm.chat_completion_stream(
     ],
     tool_definitions = openai_function_to_tool.generate_definitions(semantic_kernel_v0_to_openai_function.generate_definitions([(PluginCapital(), "PluginCapital")])),
 )
+name=None
+arguments=''
 for chunk in chat_completion_stream:
     if len(chunk.choices) > 0:
         print(chunk.choices[0].message, end='\n-----\n')
-
+        if chunk.choices[0].message.tool_calls and len(chunk.choices[0].message.tool_calls) > 0:
+            if (name is None) and (chunk.choices[0].message.tool_calls[0].function.name is not None):
+                name = chunk.choices[0].message.tool_calls[0].function.name
+            if chunk.choices[0].message.tool_calls[0].function.arguments is not None:
+                arguments += chunk.choices[0].message.tool_calls[0].function.arguments
+assert 'PluginCapital' in name
+assert 'brazil' in arguments.lower()
 
 # chat completion, normal, async
 chat_completion = await llm.chat_completion_async(
@@ -428,7 +502,8 @@ chat_completion = await llm.chat_completion_async(
     tool_definitions = openai_function_to_tool.generate_definitions(semantic_kernel_v0_to_openai_function.generate_definitions([(PluginCapital(), "PluginCapital")])),
 )
 print(chat_completion.choices[0].message.tool_calls[0].dict())
-
+assert len(chat_completion.choices[0].message.tool_calls) > 0
+assert 'PluginCapital' in chat_completion.choices[0].message.tool_calls[0].function.name
 
 # chat completion, stream, async
 chat_completion_stream = llm.chat_completion_stream_async(
@@ -438,7 +513,16 @@ chat_completion_stream = llm.chat_completion_stream_async(
     ],
     tool_definitions = openai_function_to_tool.generate_definitions(semantic_kernel_v0_to_openai_function.generate_definitions([(PluginCapital(), "PluginCapital")])),
 )
+name=None
+arguments=''
 async for chunk in chat_completion_stream:
     if len(chunk.choices) > 0:
         print(chunk.choices[0].message, end='\n-----\n')
+        if chunk.choices[0].message.tool_calls and len(chunk.choices[0].message.tool_calls) > 0:
+            if (name is None) and (chunk.choices[0].message.tool_calls[0].function.name is not None):
+                name = chunk.choices[0].message.tool_calls[0].function.name
+            if chunk.choices[0].message.tool_calls[0].function.arguments is not None:
+                arguments += chunk.choices[0].message.tool_calls[0].function.arguments
+assert 'PluginCapital' in name
+assert 'brazil' in arguments.lower()
 '''
