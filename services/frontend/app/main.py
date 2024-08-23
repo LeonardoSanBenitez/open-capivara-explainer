bf7fad81aae1ce0e97994844c8f9a52e0969b719
Leonardo Benitez <lsbenitezpereira@gmail.com>
2024-08-23 17:31:23 +0300

    frontend: show intermediate results

--- services/frontend/app/main.py
+++ services/frontend/app/main.py
@@ -5,7 +5,9 @@ import time
 import requests
 from typing import List
 from typing import Generator
-from libs.plugin_orchestrator.answer_validation import ValidatedAnswerPart
+from libs.plugin_orchestrator.answer_validation import ValidatedAnswerPart, IntermediateResult
+import pandas as pd
+
 
 # Sample data
 alert_ids = ["dc2a", "981a", "b3f7"]
@@ -50,17 +52,79 @@ def response_generator() -> Generator[str, None, None]:
     '''
 
     responses: List[ValidatedAnswerPart] = [
-        ValidatedAnswerPart(answer='Hello '),
-        ValidatedAnswerPart(answer='there!'),
-        ValidatedAnswerPart(answer=' How '),
-        ValidatedAnswerPart(answer='can I '),
-        ValidatedAnswerPart(answer='assist'),
-        ValidatedAnswerPart(answer=' you'),
-        ValidatedAnswerPart(answer=' today'),
-        ValidatedAnswerPart(answer='?'),
+        ValidatedAnswerPart(answer=None, citations=None, visualizations=None, intermediate_results=[IntermediateResult(step=1, status_code=200, function_name='PluginSAP_get_purchase_orders', function_arguments={}, result='[{"deliveryDate": "2021-01-01", "totalAmount": 10000, "items": [{"materialId": "IPS Natural Die Material ND1", "quantity": 1}, {"materialId": "Untersuchungshandschuhe Nitril light lemon Gr. M", "quantity": 10}, {"materialId": "Antiseptica r.f.u. H\\u00e4ndedesinfektion Flasche 1 Liter", "quantity": 1}]}]', message='At step 1, executing PluginSAP_get_purchase_orders', timestamp=1724422907, citation_id='d01b7fd7')]),
+        ValidatedAnswerPart(answer=None, citations=None, visualizations=None, intermediate_results=[IntermediateResult(step=2, status_code=200, function_name='PluginSAP_create_purchase_order', function_arguments={'amount': 100, 'material_id': 'Untersuchungshandschuhe Nitril light lemon Gr. M'}, result='Order placed successfully', message='At step 2, executing PluginSAP_create_purchase_order', timestamp=1724422907, citation_id='ed7a943b')]),
+        ValidatedAnswerPart(answer=None, citations=None, visualizations=None, intermediate_results=[IntermediateResult(step=3, status_code=200, function_name='PluginServiceNow_close_ticket', function_arguments={}, result='Ticket closed successfully', message='At step 3, executing PluginServiceNow_close_ticket', timestamp=1724422908, citation_id='64e0a3b0')]),
+        ValidatedAnswerPart(answer='{"', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='text', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='":"', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='I', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' have', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' placed', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' an', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' order', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' for', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' ', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='100', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' units', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' of', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' the', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=" '", citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='Unt', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='ers', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='uch', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='ung', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='sh', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='ands', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='chu', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='he', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' N', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='itr', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='il', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' light', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' lemon', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' Gr', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='.', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' M', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer="'", citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' glove', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' model', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=',', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' as', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' requested', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='.', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' Additionally', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=',', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' I', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' have', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' closed', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' the', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' ticket', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' regarding', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' the', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' defective', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' product', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='.', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' Is', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' there', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' anything', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' else', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' I', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' can', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' assist', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' you', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer=' with', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='?"', citations=None, visualizations=None, intermediate_results=None),
+        ValidatedAnswerPart(answer='}', citations=None, visualizations=None, intermediate_results=None),
     ]
     for response in responses:
-        yield response.answer
+        if response.answer is not None:
+            yield response.answer
+            #yield pd.DataFrame([['lala', {'mua': response.answer}]], columns=['name', 'args'])# {'lalala': response.answer}
+        if (response.intermediate_results is not None) and (len(response.intermediate_results) > 0):
+            r = response.intermediate_results[0]
+            yield f':gray[_At step {r.step}, the function `{r.function_name}` was called by the agent. Check details below._]'
+            yield {'Arguments': r.function_arguments, 'Result code': r.status_code, 'Result': r.result}
         time.sleep(0.05)
 
 #########################################################
@@ -109,4 +173,5 @@ with conversation_copilot:
         # assistant response
         response = container_copilot.chat_message("assistant").write_stream(response_generator())
         selected_alert['conversation_copilot'].append({"user": "assistant", "message": response})
-        capivara_update_conversation_copilot(selected_alert_id, selected_alert['conversation_copilot'])
+        # TODO: the backend should be updated based on the original ValidatedAnswer, that contains all structured details
+        #capivara_update_conversation_copilot(selected_alert_id, selected_alert['conversation_copilot'])
